ec752e9cee393ec95db571c6e66a5baf6ec20cbf
lilizarr <lilizarr@student.42wolfsburg.de>
2024-12-08 22:26:01 +0100

    [ft_irc/Victoria] * Fix: 	fix message line in on_handshake hooke
    	-Modified files:
    		src/server/Server.cpp
    		testScripts/handshake.py
    		testScripts/users_created.txt
    ~ Autor: Victoria Lizarraga (@VictoriaLizCor / @lilizarr) ~
    Collaborators: @Gaspar24, @MemoCSales

--- testScripts/handshake.py
+++ testScripts/handshake.py
@@ -45,32 +45,16 @@ def hexChatMsg():
 		hexchat.prnt(f"{GREEN}User name: {user}{ENDC}")
 		hexchat.prnt(f"{GREEN}Real name: {realname}{ENDC}")
 		hexchat.prnt(f"{GREEN}Context ID: {context_id}{ENDC}")
-		hexchat.hook_timer(700, send_message, nick)
+		hexchat.hook_timer(1000, send_message, nick)
 	else:
 		hexchat.prnt(f"{RED}Failed to get current context{ENDC}")
 	return False
 #--------------------------------------------------------------------------------
 def on_handshake(word, word_eol, userdata):
-	return hexchat.EAT_NONE
-#--------------------------------------------------------------------------------
-# def on_server_message0(word, word_eol, userdata):
-# 	message = word_eol[0]
-# 	if "001" in message:
-# 		hexchat.prnt(f"{LIGHT_GREY}------------- HANDSHAKE WITH SERVER ------------- {ENDC}")
-# 	elif "NOTICE" in message:
-# 		hexchat.prnt(f"{YELLOW}{message}{ENDC}")
-# 	elif "ERROR" in message:
-# 		hexchat.prnt(f"{RED}{message}{ENDC}")
-# 	else:
-# 		hexchat.prnt(f"{LIGHT_GREY}**********{ENDC}")
-# 	return hexchat.EAT_NONE
-
-
-def on_server_message(word, word_eol, userdata):
 	global connection_established
 	global message
 	if word_eol:
-		message += ''.join(word_eol[0]) + "\n"
+		message += "\t" + ''.join(word_eol[0])
 		if "NOTICE" in word_eol[0]:
 			hexchat.prnt(f"{YELLOW}NOTICE{ENDC}")
 		elif "ERROR" in word_eol[0]:
@@ -79,33 +63,22 @@ def on_server_message(word, word_eol, userdata):
 			if not connection_established:
 				hexchat.prnt(f"{LIGHT_GREY}------------- HANDSHAKE WITH SERVER -------------{ENDC}")
 				connection_established = True
-			# message = word_eol[0]
-			char_count = len(message)
-			try:
-				message = message.encode('ascii', 'ignore').decode('ascii')
-			except UnicodeEncodeError:
-				message = message  # Fallback to original message if encoding fails
-			
-			char_count = len(message)
-			hexchat.emit_print("Server Text", "\t {}".format(message))
+			for line in message and "\n" in line:
+				hexchat.emit_print("Server Text", f"{line}")
+			hexChatMsg()
+			# try:
+			# 	message = message.encode('ascii', 'ignore').decode('ascii')
+			# except UnicodeEncodeError:
+			# 	message = message  # Fallback to original message if encoding fails
+			# char_count = len(message)
 			# print("{} (len:{})".format(message, char_count))
 			# hexchat.prnt(f"{message} ({char_count} |eol: {len(word_eol)} | word: {len(word)} )")
-
-	# print("wordLen:", len(word))
-	# print("word:", word)
-	# print("word_eol:", word_eol)
-	# if len(word) > 1:
-	# 	if "NOTICE" in word_eol[0]:
-	# 		hexchat.prnt(f"{YELLOW}NOTICE{ENDC}")
-	# 	elif "ERROR" in word_eol[0]:
-	# 		hexchat.prnt(f"{RED}ERROR{ENDC}")
-	# 	hexChatMsg()
 
 	return hexchat.EAT_ALL
 #--------------------------------------------------------------------------------
 # Hook into server messages
-hexchat.hook_server("RAW LINE", on_server_message)
-# hexchat.hook_print("Server Text", on_server_message)
+hexchat.hook_server("RAW LINE", on_handshake)
+# hexchat.hook_print("Server Text", on_handshake)
 #--------------------------------------------------------------------------------
 # hexchat.hook_server("001", on_handshake)
 # print(__module_name__, "version", __module_version__, "loaded.")
