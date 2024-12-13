26fe7810c86063c7378c18c8ffa9f1cd3c9bcb46
lilizarr <lilizarr@student.42wolfsburg.de>
2024-12-13 18:21:13 +0100

    [ft_irc/V_printMutex] * Fix: 	update in defaultClient, 	check with main branch if file descriptors close
    	-Modified files:
    		testScripts/defaultClient.sh
    ~ Autor: Victoria Lizarraga (@VictoriaLizCor / @lilizarr) ~
    Collaborators: @Gaspar24, @MemoCSales

--- testScripts/defaultClient.sh
+++ testScripts/defaultClient.sh
@@ -11,10 +11,11 @@ CHANNEL="#YourChannel"
 MESSAGE="Hello, IRC!"
 
 # Function to handle signal interruptions
-cleanup() {
-    echo "Signal received. Cleaning up..."
-    pkill -P $$ nc
-    exit 0
+cleanup()
+{
+	echo "Signal received. Cleaning up..."
+	pkill -P $$ nc
+	exit 0
 }
 
 # Set the trap to catch SIGINT (Ctrl+C)
@@ -22,32 +23,34 @@ trap cleanup SIGINT
 
 # Connect to the IRC server
 (
-    sleep 1
-    echo "PASS $PASSWORD"
-    sleep 1
-    echo "NICK $NICK"
-    sleep 1
-    echo "USER $USER 0 * :$REALNAME"
-    sleep 1
-    # echo "PRIVMSG $CHANNEL :$MESSAGE"
-    # echo "JOIN $CHANNEL"
-    # sleep 1
-    # sleep 1
-    # echo "QUIT"
-    # Connect to the IRC server
-    while true; do
-        if ! read -p "" input; then
-            echo "Read error: $?"
-            break
-        fi
-        echo "$input"
-        # Break the loop if the input is "QUIT"
-        if [ "$input" == "QUIT" ]; then
-            echo "QUIT :Client exiting"
-            pkill -P $$ nc
-            break
-        fi
-    done
-    exec 1>&-
+	sleep 1
+	echo "PASS $PASSWORD"
+	sleep 1
+	echo "NICK $NICK"
+	sleep 1
+	echo "USER $USER 0 * :$REALNAME"
+	sleep 1
+	# echo "PRIVMSG $CHANNEL :$MESSAGE"
+	# echo "JOIN $CHANNEL"
+	# sleep 1
+	# sleep 1
+	# echo "QUIT"
+	# Connect to the IRC server
+	while true; do
+		if ! read -p "" input <&0; then
+			if [ $? -eq 1 ]; then
+				pkill -P $$ nc
+			fi
+			break
+		fi
+		echo "$input"
+		# Break the loop if the input is "QUIT"
+		if [ "$input" == "QUIT" ]; then
+			echo "QUIT :Client exiting"
+			pkill -P $$ nc
+			break
+		fi
+	done
+	exec 1>&-
 ) | nc $SERVER $PORT &
 wait $!
